86df74d5dfd2f09ccc7b075d46b18f9fb26e7419
vijayyagik1 <lvyagik@gmail.com>
2023-03-06 23:41:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>I am heading one...from first component</h1>
+      <h2>I am heading two...from second component</h2>
+      <h3>I am heading three...from third component</h3>
+      <button>Click Me</button>
     </div>
+   
   );
 }
 
---
+++ src/App2.js
@@ -0,0 +1,34 @@
+export default function Challenge(){
+    return(
+        <>
+ <One/>
+ <Second/>
+ <Third/>
+ <Fourth/>
+ </>
+    )
+}
+
+function One(){
+    return (
+        <h1>I am heading one...from first component</h1>
+    )
+}
+
+function Second(){
+    return (
+        <h2>I am heading Second...from Second component</h2>
+    )
+}
+
+function Third(){
+    return (
+        <h3>I am heading third...from third component</h3>
+    )
+}
+
+function Fourth(){
+    return (
+        <button>Click me</button>
+    )
+}
